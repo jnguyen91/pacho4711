3ef71815000ca0f63956111ea3b239ed1d59b415
Jeffrey Nguyen <jeffnguyen91@hotmail.com>
2017-09-07 13:16:55 -0700

    Added studednt class

--- index.php
+++ index.php
@@ -12,7 +12,46 @@ and open the template in the editor.
     <body>
         <?php
         // put your code here
-        echo 'where do broken hearts go';
+        include('Student.php');
+
+$students = array();
+
+$first = new Student();
+$first->surname = "Doe";
+$first->first_name = "John";
+$first->add_email('home','john@doe.com');
+$first->add_email('work','jdoe@mcdonalds.com');
+$first->add_grade(65);
+$first->add_grade(75);
+$first->add_grade(55);
+$students['j123'] = $first;
+
+$second = new Student();
+$second->surname = "Einstein";
+$second->first_name = "Albert";
+$second->add_email('home','albert@braniacs.com');
+$second->add_email('work1','a_einstein@bcit.ca');
+$second->add_email('work2','albert@physics.mit.edu');
+$second->add_grade(95);
+$second->add_grade(80);
+$second->add_grade(50);
+$students['a456'] = $second;
+
+$third = new Student();
+$third->surname = "Nguyen";
+$third->first_name = "Jeffrey";
+$third->add_email('home','jeffnguyen91@hotmail.com');
+$third->add_email('work1','jnguyen52@my.bcit.ca');
+$third->add_email('work2','jeffrey.nguyen@chatnels.com');
+$third->add_grade(50);
+$third->add_grade(51);
+$third->add_grade(50);
+$students['v231'] = $third;
+
+ksort($students);
+
+foreach($students as $student)
+    echo $student->toString();
         ?>
     </body>
 </html>
