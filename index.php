1559147ef6d056564401a2653d79cb6ef7d42ed3
Jeffrey Nguyen <jeffnguyen91@hotmail.com>
2017-09-07 14:04:25 -0700

    index.php comment updates

--- index.php
+++ index.php
@@ -11,47 +11,58 @@ and open the template in the editor.
     </head>
     <body>
         <?php
-        // put your code here
+        
+        #links the student.php
         include('Student.php');
 
-$students = array();
+        
+        $students = array();
 
-$first = new Student();
-$first->surname = "Doe";
-$first->first_name = "John";
-$first->add_email('home','john@doe.com');
-$first->add_email('work','jdoe@mcdonalds.com');
-$first->add_grade(65);
-$first->add_grade(75);
-$first->add_grade(55);
-$students['j123'] = $first;
+        #create new student named John Doe, email, and grade
+        $first = new Student();
+        $first->surname = "Doe";
+        $first->first_name = "John";
+        $first->add_email('home','john@doe.com');
+        $first->add_email('work','jdoe@mcdonalds.com');
+        $first->add_grade(65);
+        $first->add_grade(75);
+        $first->add_grade(55);
+        $students['j123'] = $first;
 
-$second = new Student();
-$second->surname = "Einstein";
-$second->first_name = "Albert";
-$second->add_email('home','albert@braniacs.com');
-$second->add_email('work1','a_einstein@bcit.ca');
-$second->add_email('work2','albert@physics.mit.edu');
-$second->add_grade(95);
-$second->add_grade(80);
-$second->add_grade(50);
-$students['a456'] = $second;
+        #create a new student named Albert Einstein with Email and grades
+        $second = new Student();
+        $second->surname = "Einstein";
+        $second->first_name = "Albert";
+        $second->add_email('home','albert@braniacs.com');
+        $second->add_email('work1','a_einstein@bcit.ca');
+        $second->add_email('work2','albert@physics.mit.edu');
+        $second->add_grade(95);
+        $second->add_grade(80);
+        $second->add_grade(50);
+        $students['a456'] = $second;
 
-$third = new Student();
-$third->surname = "Nguyen";
-$third->first_name = "Jeffrey";
-$third->add_email('home','jeffnguyen91@hotmail.com');
-$third->add_email('work1','jnguyen52@my.bcit.ca');
-$third->add_email('work2','jeffrey.nguyen@chatnels.com');
-$third->add_grade(50);
-$third->add_grade(51);
-$third->add_grade(50);
-$students['v231'] = $third;
+        #Creating third student named Jeffrey Nguyen, With emails and grade
+        #it wil printout the average of the three grades
+        #Jeffrey is a horrible student and barely passses his courses
+        $third = new Student();
+        $third->surname = "Nguyen";
+        $third->first_name = "Jeffrey";
+        $third->add_email('home','jeffnguyen91@hotmail.com');
+        $third->add_email('work1','jnguyen52@my.bcit.ca');
+        $third->add_email('work2','jeffrey.nguyen@chatnels.com');
+        $third->add_grade(50);
+        $third->add_grade(51);
+        $third->add_grade(50);
+        $students['v231'] = $third;
 
-ksort($students);
+        #ksort sorts an associative array in ascending order, according to the 
+        #key
+        
+        ksort($students);
 
-foreach($students as $student)
-    echo $student->toString();
+        #Will printout the students surname, firstname, emails and average grade
+        foreach($students as $student)
+             echo $student->toString();
         ?>
     </body>
 </html>
