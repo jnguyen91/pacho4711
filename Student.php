29a8da315ea03e49fa673ed54ce04caa3a088c5b
Jeffrey Nguyen <jeffnguyen91@hotmail.com>
2017-09-07 14:04:50 -0700

    student.php comment update

--- Student.php
+++ Student.php
@@ -12,11 +12,8 @@
  * @author jeffn
  */
 class Student {
-    //put your code here
     
-    
-    
-    
+    #constructor for surname, firstname, email and grades
     function __construct() {
     $this->surname = '';
     $this->first_name = '';
@@ -24,28 +21,30 @@ class Student {
     $this->grades = array();
 
     }
+    #email function adds email address to either work/home email address
     function add_email($which,$address) {
     $this->emails[$which] = $address;
-}
-
-function add_grade($grade) {
-    $this->grades[] = $grade;
-}
-function average() {
-    $total = 0;
-    foreach ($this->grades as $value)
-        $total += $value;
-    return $total / count($this->grades);
-}
-function toString() {
-    $result = $this->first_name . ' ' . $this->surname;
-    $result .= ' ('.$this->average().")\n";
-    foreach($this->emails as $which=>$what)
-        $result .= $which . ': '. $what. "\n";
-    $result .= "\n";
-    return '<pre>'.$result.'</pre>';
-}
-
-
+    }
     
+    #add grade function 
+    function add_grade($grade) {
+        $this->grades[] = $grade;
+    }
+    #will calculate the average of the grades and return it as grades
+    function average() {
+        $total = 0;
+        foreach ($this->grades as $value)
+            $total += $value;
+        return $total / count($this->grades);
+    }
+    #format to display first name, last name and average grade in parenthesis ()
+    #new line for individual emails
+    function toString() {
+        $result = $this->first_name . ' ' . $this->surname;
+        $result .= ' ('.$this->average().")\n";
+        foreach($this->emails as $which=>$what)
+            $result .= $which . ': '. $what. "\n";
+        $result .= "\n";
+        return '<pre>'.$result.'</pre>';
+    }
 }
